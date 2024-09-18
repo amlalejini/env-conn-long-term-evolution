ba5752b78b4386e68e5bd8f648f4a4053d39ec6f
Grant-Gordon <ggmain777@gmail.com>
2024-09-18 16:32:58 -0400

    updated aggregate analysis and .gitignore to ignore third-party/avida, pyEnv, and hpcc data from 24-06-12-nature2003 experiment

--- experiments/2024-06-12-nature2003/aggregate_analysis.py
+++ experiments/2024-06-12-nature2003/aggregate_analysis.py
@@ -1,17 +1,17 @@
 import polars as pl
 from matplotlib import pyplot as plt
 def main():
-    df = pl.read_csv("./aggregate_data_out/aggregate.csv")
-
+    df = pl.read_csv("./aggregated_data_out/aggregate.csv")
+    numDef9EvolvedBothEvals = df.filter((pl.col('treatment') == 'default_9') & (pl.col('EQU_evolved_in_tasks')) & (pl.col('EQU_evolved_in_mostCPUsGenotype') == True)).shape[0]
     #passed both evaulations (tasks && mostCPUSGenotype evolved)
-    numDef9EvolvedBothEvals = df.filter(pl.col('treatment') == 'default_9' & pl.col('evolved_in_tasks') == 'True' & pl.col('evolved_in_mostCPUsGenotype') == 'True')
-    numEQUOnlyEvolvedBothEvals = df.filter(pl.col('treatment') == 'EQU_Only' & pl.col('evolved_in_tasks') == 'True' & pl.col('evolved_in_mostCPUsGenotype') == 'True')
+    numEQUOnlyEvolvedBothEvals = df.filter((pl.col('treatment') == 'EQU_Only') & (pl.col('EQU_evolved_in_tasks') == True) & (pl.col('EQU_evolved_in_mostCPUsGenotype') == True)).shape[0]
+    
 
-    numDef9EvolvedTasks = df.filter(pl.col('treatment') == 'default_9' & pl.col('evolved_in_tasks') == 'True')
-    numDef9EvolovedMostCPUs = df.filter(pl.col('treatment') == 'default_9' & pl.col('evolved_in_mostCPUsGenotype') == 'True')
+    # numDef9EvolvedTasks = df.filter(pl.col('treatment') == 'default_9' & pl.col('evolved_in_tasks') == True)
+    # numDef9EvolovedMostCPUs = df.filter(pl.col('treatment') == 'default_9' & pl.col('EQU_evolved_in_mostCPUsGenotype') == True)
 
-    numEQUOnlyEvolvedTasks = df.filter(pl.col('treatment') == 'EQU_only' & pl.col('evolved_in_tasks') == 'True')
-    numEQUOnlyEvolovedMostCPUs = df.filter(pl.col('treatment') == 'EQU_only' & pl.col('evolved_in_mostCPUsGenotype') == 'True')
+    # numEQUOnlyEvolvedTasks = df.filter(pl.col('treatment') == 'EQU_only' & pl.col('evolved_in_tasks') == True)
+    # numEQUOnlyEvolovedMostCPUs = df.filter(pl.col('treatment') == 'EQU_only' & pl.col('EQU_evolved_in_mostCPUsGenotype') == True)
 
 
     plt.bar =(['Defualt9', 'EQU_Only'], [numDef9EvolvedBothEvals, numEQUOnlyEvolvedBothEvals])
@@ -19,6 +19,6 @@ def main():
     plt.xlabel('treatments')
     plt.ylabel('Number of Runs EQU evolved')
 
-    plt.show
+    plt.show()
 if __name__ == "__main__":
     main()
