656723196a6c0e6e4d5d8f183d5c1df2d13ee102
Grant-Gordon <ggmain777@gmail.com>
2024-09-28 22:49:02 -0400

    bug-fixed and tested local slurm generation. should be ready to run on hpcc

--- experiments/2024-09-26-exploratory-runs/hpc/job-gen.sh
+++ experiments/2024-09-26-exploratory-runs/hpc/job-gen.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 REPLICATES=50
-EXP_SLUG=2024-09-11-vary-struct
-ACCOUNT=devolab
+EXP_SLUG=2024-09-26-exploratory-runs
 SEED_OFFSET=1000
-JOB_TIME=24:00:00
+JOB_TIME=32:00:00
 JOB_MEM=8G
 PROJECT_NAME=env-conn-long-term-evolution
 RUNS_PER_SUBDIR=950
 
-REPO_DIR=/mnt/home/lalejini/devo_ws/${PROJECT_NAME}
+REPO_DIR=/mnt/home/gordongr/research/${PROJECT_NAME}
 REPO_SCRIPTS_DIR=${REPO_DIR}/scripts
 HOME_EXP_DIR=${REPO_DIR}/experiments/${EXP_SLUG}
 GRAPHS_DIR=${HOME_EXP_DIR}/hpc/spatial-structs
@@ -17,7 +16,7 @@ GRAPHS_CFG=${HOME_EXP_DIR}/hpc/graphs.json
 EVENTS_DIR=${HOME_EXP_DIR}/hpc/events
 PARAM_SNAPSHOT_DIR=${HOME_EXP_DIR}/hpc/param_snapshots
 
-DATA_DIR=/mnt/scratch/lalejini/data/${PROJECT_NAME}/${EXP_SLUG}
+DATA_DIR=/mnt/projects/lalejina_project/data-gordongr/${PROJECT_NAME}/${EXP_SLUG}
 JOB_DIR=${DATA_DIR}/jobs
 CONFIG_DIR=${HOME_EXP_DIR}/hpc/config
 
@@ -38,6 +37,5 @@ python3 gen-sub.py \
   --repo_dir ${REPO_DIR} \
   --replicates ${REPLICATES} \
   --job_dir ${JOB_DIR} \
-  --hpc_account ${ACCOUNT} \
   --seed_offset ${SEED_OFFSET} \
   --spatial_structs_dir ${GRAPHS_DIR}
