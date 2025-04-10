a5ff1f6940bcdac2e278a4a3290f7bbf8a5a6ef1
Alexander Lalejini <amlalejini@gmail.com>
2025-04-10 12:24:33 -0400

    add clipper env script

---
+++ hpc-env/clipper-hpc-env.sh
@@ -0,0 +1,9 @@
+PROJECT_NAME=env-conn-long-term-evolution
+REPO_DIR=/mnt/home/lalejina/devo_ws/${PROJECT_NAME}
+REPO_SCRIPTS_DIR=${REPO_DIR}/scripts
+
+module purge
+module load Python/3.11.9
+module load gcc/14.2.0
+module load CMake/3.30.5
+source ${REPO_DIR}/pyenv/bin/activate
