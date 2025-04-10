286a2acb233ba0a04a01568a90a3ce391c73f0fa
Alexander Lalejini <amlalejini@gmail.com>
2025-04-10 12:25:39 -0400

    Fix python module name

--- hpc-env/clipper-hpc-env.sh
+++ hpc-env/clipper-hpc-env.sh
@@ -3,7 +3,7 @@ REPO_DIR=/mnt/home/lalejina/devo_ws/${PROJECT_NAME}
 REPO_SCRIPTS_DIR=${REPO_DIR}/scripts
 
 module purge
-module load Python/3.11.9
+module load python/3.11.9
 module load gcc/14.2.0
 module load CMake/3.30.5
 source ${REPO_DIR}/pyenv/bin/activate
